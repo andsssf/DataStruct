c8e6a85222db38120b506dc13acfe220ed990370
andsssf <70904338+andsssf@users.noreply.github.com>
2021-08-10 07:01:03 -0700

    二分查找

--- DS_HSearchAlgorithms.h
+++ DS_HSearchAlgorithms.h
@@ -9,3 +9,19 @@ int seqSearch(const std::vector<int> & table, int key) {
     }
     return -1;
 }
+
+// 二分查找
+int binarySearch(const std::vector<int> & table, int key) {
+    int low = 0;
+    int high = table.size() - 1;
+    while (low <= high) {
+        int mid = (low + high) / 2;
+        if (table[mid] == key) return mid;
+        else if (key > table[mid]) {
+            low = mid + 1;
+        } else {
+            high = mid - 1;
+        }
+    }
+    return -1;
+}
